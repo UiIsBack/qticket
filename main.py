9b25e496f6069b99014fe1e843632a0eb440e3ee
UiIsBack <100056544+UiIsBack@users.noreply.github.com>
2022-06-04 01:51:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,124 @@
+import asyncio
+from fileinput import close
+from nextcord import TextChannel
+import nextcord, json
+from nextcord.ext import commands
+
+
+
+client = commands.Bot(command_prefix=">")
+
+@client.event
+async def on_ready():
+    print("[+]")
+
+class Close(nextcord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+
+    @nextcord.ui.button(label="Close ticket", style=nextcord.ButtonStyle.red)
+    async def close(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        await interaction.response.send_message("Closing ticket", ephemeral=True); await asyncio.sleep(2)
+        await interaction.channel.delete()
+        with open("config.json", "r+") as f:
+            t = json.load(f)
+        t['num'] = t['num'] - 1
+        json.dump(t, open("config.json", "w"), indent = 4)
+        with open("config.json", "r") as e:
+            d = json.load(e)
+        md = d['staff-log']
+        b = client.get_channel(md)
+        s = nextcord.Embed(title="Log's", description=f"""  Ticket Closed
+                                                            Ticket: #{interaction.channel}
+                                                            User: {interaction.user.mention}
+                                                            """, color=nextcord.Color.red())
+        await b.send(embed=s)
+        
+class Confirm(nextcord.ui.View):
+    def __init__(self):
+        super().__init__()
+  
+
+    @nextcord.ui.button(label="Create ticket", style=nextcord.ButtonStyle.blurple)
+    async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+
+
+      
+        with open("config.json", "r+") as f:
+            t = json.load(f)
+        t['num'] = t['num'] + 1
+        json.dump(t, open("config.json", "w"), indent = 4)
+        numt = t['num']
+        members = client.get_all_members
+
+        guild = client.get_guild(982392273985732629); channel = await guild.create_text_channel(f"{numt}-ticket")
+        await interaction.response.send_message("Created ticket", ephemeral=True)
+        await channel.set_permissions(guild.default_role, view_channel=False)
+        await channel.set_permissions(interaction.user, view_channel=True)
+        with open("config.json", "r") as e:
+            d = json.load(e)
+        md = d['staff-log']
+        b = client.get_channel(md)
+        s = nextcord.Embed(title="Log's", description=f"""  Ticket created
+                                                            Ticket: <#{channel.id}>
+                                                            User: {interaction.user.mention}
+                                                            """, color=nextcord.Color.green())
+        await b.send(embed=s)
+        embed = nextcord.Embed(title="Tickets", description="Welcome, how can we help you today?", color=nextcord.Color.blurple())
+        view = Close()
+        await channel.send(f"{interaction.user.mention}", embed=embed, view=view)
+        await view.wait()
+        
+        
+
+
+@client.command()
+@commands.has_permissions(administrator=True)
+
+async def setup(ctx):
+
+    view = Confirm()
+    embed= nextcord.Embed(title='Tickets', description="Press the button below to crerate ticket!", color=nextcord.Color.blurple())
+    await ctx.send(embed=embed, view=view)
+
+    await view.wait()
+@client.command()
+async def closeall(ctx):
+    f = nextcord.Embed(title="Tickets", description="Closing all tickets", color=nextcord.Color.blurple())  
+    await ctx.send(embed = f)
+    channels = ctx.guild.channels
+    for channel in channels:
+        name = channel.name
+        if str(name).endswith("-ticket") or str(name).startswith("ticket-"):
+            await channel.delete()
+    with open("config.json", "r+") as f:
+                t = json.load(f)
+    t['num'] = 0
+    ee = nextcord.Embed(title="Tickets", description="all tickets closed", color=nextcord.Color.green())  
+    await ctx.send(embed = ee)
+@client.command()
+async def close(ctx):
+        await ctx.send_message("Closing ticket", ephemeral=True); await asyncio.sleep(2)
+        await ctx.channel.delete()
+        with open("config.json", "r+") as f:
+            t = json.load(f)
+        t['num'] = t['num'] - 1
+        json.dump(t, open("config.json", "w"), indent = 4)
+        with open("config.json", "r") as e:
+            d = json.load(e)
+        md = d['staff-log']
+        b = client.get_channel(md)
+        s = nextcord.Embed(title="Log's", description=f"""  Ticket Closed
+                                                            Ticket: #{ctx.channel}
+                                                            User: {ctx.user.mention}
+                                                            """, color=nextcord.Color.red())
+        b.send(embed=s)
+@client.command()
+async def cmds(ctx):
+    o = nextcord.Embed(title="Qticket", description="prefix = >\n>setup setups ticket sys in that channel\n>closeall closes all tickets\n>close closes current ticket", color=nextcord.Color.blurple())
+    await ctx.send(embed = o)
+with open("config.json", "r") as a:
+    k = json.load(a)
+token = k['token']
+client.run(token)
---
+++ readme.md
@@ -0,0 +1,16 @@
+## Qticket
+
+
+# How to use:
+1. download the file
+2. extract the zip
+3. install python (3.9.5 preferabbly) [Here](https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe)
+4. open config.json 
+5. ignore the num section go down to where it says staff-log and replace "channel_id here" with your channel id to recieve mod logs
+6. go to the token section in config and replace *token_here* with your [bot-token](https://discord.com/developers/applications)
+7. invite your bot to the server u want the ticket sys in 
+8. open install.bat file
+9. open main.py file
+10. make sure bot has admin in server
+11. do >setup to setup ticket sys 
+12. enjoy for any support join my discord server [here](https://discord.gg/gui)
